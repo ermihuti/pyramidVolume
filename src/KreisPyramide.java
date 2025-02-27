5a176777d5a781d0472b27ca768372f6e7c4719d
EriPuk19 <eripuk19@htl-shhkoder.com>
2025-02-27 11:53:44 +0100

    Pyramid Volume

--- src/KreisPyramide.java
+++ src/KreisPyramide.java
@@ -1,14 +1,23 @@
 class KreisPyramide extends Pyramide {
     private double radius;
+    private double seitenkante;
 
-    public KreisPyramide(double radius, double hohe) {
+    public KreisPyramide(double radius, double hohe, double seitenkante) {
         super(hohe);
         this.radius = radius;
+        this.seitenkante = seitenkante;
     }
 
     @Override
     public double volumen() {
         double grundflache = Math.PI * radius * radius;
         return (1.0 / 3) * grundflache * getHohe();
+    }
+
+    @Override
+    public double oberflache() {
+        double grundflache = Math.PI * radius * radius;
+        double mantelflache = Math.PI * radius * seitenkante;
+        return grundflache + mantelflache;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         QuadratischePyramide quadratischePyramide  = new QuadratischePyramide(4, 6);
         System.out.println("Quadratischepyramide  Volumen: " + quadratischePyramide.volumen());
 
-        KreisPyramide kreisPyramide = new KreisPyramide(3, 6);
+        KreisPyramide kreisPyramide = new KreisPyramide(3, 6, 5);
         System.out.println("Kreispyramide Volumen: " + kreisPyramide.volumen());
+        System.out.println("Kreispyramide Oberfläche: " + kreisPyramide.oberflache());
 
         DreieckPyramide dreieckPyramide = new DreieckPyramide(5, 3, 6);
         System.out.println("Dreieckpyramide Volumen: " + dreieckPyramide.volumen());
