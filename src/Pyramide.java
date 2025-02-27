029d5abddfd4216fe813902104b58ded19afac5a
Ermi <ermhut19@htl-shkoder.com>
2025-02-27 10:57:53 +0100

    Pyramid Volume

---
+++ src/Pyramide.java
@@ -0,0 +1,13 @@
+abstract class Pyramide {
+    private double hohe;
+
+    public Pyramide(double hohe) {
+        this.hohe = hohe;
+    }
+
+    public double getHohe() {
+        return hohe;
+    }
+
+    public abstract double volumen();
+}
