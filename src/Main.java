6656950828683869a21c267f206c5f1f4a03a52b
Gesart123 <gesmll20@htl-shkoder.com>
2025-02-27 11:35:16 +0100

    Dreieck

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         DreieckPyramide dreieckPyramide = new DreieckPyramide(4, 3, 6);
         System.out.println("Dreieckpyramide Volumen: " + dreieckPyramide.volumen());
-
-        QuadratischePyramide quadratischePyramide  = new QuadratischePyramide(4, 6);
-        System.out.println("Quadratischepyramide  Volumen: " + quadratischePyramide.volumen());
     }
 }
 
